e7e1200bb62bafe79e01e29d205115a04b0c7946
akappos <angelos.kappos@kcitylabs.fr>
2021-08-24 19:57:13 +0200

    - Derived Data with computed

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -10,28 +10,53 @@
         {{ period }}
       </a>
     </span>
+    <a
+      v-for="post in posts"
+      :key="post.id"
+      class="panel-block">
+      <a>{{ post.title }}</a>
+      <div>{{ post.created.format('D MMM YY') }}</div>
+    </a>
   </nav>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, computed } from 'vue'
+import moment from 'moment'
+import { today, thisWeek, thisMonth } from '@/mocks'
 
 type Period = 'Today' | 'This Week' | 'This Month'
 
 export default defineComponent({
   name: 'Timeline',
   setup () {
-    // eslint-disable-next-line no-undef
     const periods = ['Today', 'This Week', 'This Month']
     // make period it, reactive
     const currentPeriod = ref<Period>('Today')
+    const posts = computed(() => {
+      return [today, thisWeek, thisMonth].filter(post => {
+        // currentPeriod is the ref
+        console.log(currentPeriod.value)
+        if (currentPeriod.value === 'Today') {
+          // if post is after yesterday's post
+          return post.created.isAfter(moment().subtract(1, 'day'))
+        }
+        if (currentPeriod.value === 'This Week') {
+          return post.created.isAfter(moment().subtract(1, 'week'))
+        }
+        if (currentPeriod.value === 'This Month') {
+          return post.created.isAfter(moment().subtract(1, 'month'))
+        }
+        return false
+      })
+    })
 
     const setPeriod = (period: Period) => {
       currentPeriod.value = period
     }
 
     return {
-      // posts,
+      posts,
       periods,
       setPeriod,
       currentPeriod
